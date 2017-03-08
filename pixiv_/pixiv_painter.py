651a9eb1ab960c5261e224280a62682b8e154d82
Nowaki <550473417@qq.com>
2017-03-08 20:36:30 +0800

    Update pixiv_painter.py

--- pixiv_/pixiv_painter.py
+++ pixiv_/pixiv_painter.py
@@ -6,38 +6,38 @@ base_url = 'http://www.pixiv.net/member_illust.php'
 seed_url = 'http://www.pixiv.net/member_illust.php?id='
 
 
-class Pixiv_Painter(object):
+class PixivPainter(object):
     def __init__(self, painter_id):
         self.painter_id = painter_id
-        self.painter_url = seed_url + str(self.painter_id)
 
-    def get_pictures_id(self, url):
+    def get_pictures_id(self):
+        painter_url = seed_url + str(self.painter_id)
         id_ = []
         t = 1
 
-        html_source = downloader(url)
+        html_source = downloader(painter_url).text
         while t == 1:
             html_soup = BeautifulSoup(html_source, 'lxml')
             span = html_soup.find('span', class_='next')
             try:
                 next_page = span.a.get('href')
             except:
-                t = 2
+                t = 0
             pattern = re.compile('(?<=data-id=")\S*(?=">)')
             picture_id = re.findall(pattern, html_source)
             id_.extend(picture_id)
             time.sleep(2)
             if next_page is not None:
-                html_source = downloader(base_url+next_page)
+                html_source = downloader(base_url+next_page).text
             else:
                 break
-        print len(id_)
+        return id_
 
 
 if __name__ == '__main__':
 
-    pixiv = Pixiv_Painter(11246082)
-    pixiv.get_pictures_id(pixiv.painter_url)
+    pixiv = PixivPainter(11246082)
+    pixiv.get_pictures_id()
 
 
 
